9f5247f17980851be7467231ad91d5b09691a6a8
Tobias Hejdenberg <tobias.hejdenberg@cytiva.com>
2022-02-03 16:50:45 +0100

    Added process starting
    Added child process start but currently does not capture output from child, work in progress

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@
 // Evalutate: 
 // parse the input
 // create process and wait or dont wait for it to finish
+using System;
+using System.Diagnostics;
+using System.ComponentModel;
+
 
 class SimpleShell
 {
@@ -42,6 +46,8 @@ class SimpleShell
             Console.WriteLine("Background process");
         } 
 
+        StartProcessWithArgs(args[0]);
+
         foreach (string arg in args) {
             Console.WriteLine(arg);
         }
@@ -49,5 +55,28 @@ class SimpleShell
         // Start a process assuming that args[0] is a filepath and the other things parameters for that process
 
         return 1;
+    }
+
+    private static void StartProcessWithArgs(string filePath) {
+        try
+            {
+                using (Process myProcess = new Process())
+                {
+                    myProcess.StartInfo.UseShellExecute = false;
+                    myProcess.StartInfo.RedirectStandardOutput = true; // Verkar som att man också måste fixa någon streamreader som läser från 
+                    // processens standard output.
+                    myProcess.StartInfo.FileName = filePath;
+                    myProcess.StartInfo.CreateNoWindow = true;
+                    myProcess.Start();
+                    // This code assumes the process you are starting will terminate itself.
+                    // Given that it is started without a window so you cannot terminate it
+                    // on the desktop, it must terminate itself or you can do it programmatically
+                    // from this application using the Kill method.
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
     }
 }
